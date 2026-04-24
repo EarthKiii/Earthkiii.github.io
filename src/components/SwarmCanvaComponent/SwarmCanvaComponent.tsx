0d2247a05fd5d66329fd2477cc72e4c9dc3fe81f
EarthKiii <jonas.charrier1@gmail.com>
2026-04-25 00:49:11 +0200

    Fix performance issue

--- src/components/SwarmCanvaComponent/SwarmCanvaComponent.tsx
+++ src/components/SwarmCanvaComponent/SwarmCanvaComponent.tsx
@@ -1,8 +1,7 @@
-import { Canvas, useFrame } from '@react-three/fiber';
-import { PerspectiveCamera } from '@react-three/drei';
-import { InstancedRigidBodies, RapierRigidBody, BallCollider, type InstancedRigidBodyProps, Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
-import { useMemo, useRef } from 'react';
-import { Color, Euler, PCFSoftShadowMap, Quaternion, Spherical, Vector3 } from 'three';
+import { Canvas } from '@react-three/fiber';
+import { InstancedRigidBodies, RapierRigidBody, BallCollider, type InstancedRigidBodyProps, Physics, RigidBody, CuboidCollider, useBeforePhysicsStep } from "@react-three/rapier";
+import { useEffect, useMemo, useRef } from 'react';
+import { Euler, PCFSoftShadowMap, Quaternion, Spherical, Vector3 } from 'three';
 
 const SQRT_BALL_AMOUNT = 40;
 const COUNT = SQRT_BALL_AMOUNT ** 2;
@@ -11,18 +10,22 @@ const SPHERE_RADIUS = 0.1;
 const GFORCE = 6.67408e-11;
 
 const CORE_MASS = 2.0;
+const CORE_DENSITY = CORE_MASS / (1.2 * 1.2 * 1.2);
+
 const SPHERE_MASS = 1.0;
+const SPHERE_DENSITY = SPHERE_MASS / ((4/3) * Math.PI * Math.pow(SPHERE_RADIUS, 3));
+
 const OBJECT_RESTITUTION = 0.01;
 
 const GRAVITY_CENTER = new Vector3(0, 0, 0);
 
-const BG_COLOR = new Color(0x232428);
+// const BG_COLOR = new Color(0x232428);
 
 const _pos = new Vector3();
 const _impulse = new Vector3();
 const _q = new Quaternion();
 
-const SwarmCanvaComponentBody = () => { 
+const SwarmScene = () => {
   const coreRef = useRef<RapierRigidBody | null>(null);
   const coreRotationEuler = useRef(new Euler(45, 45, 45));
 
@@ -38,6 +41,8 @@ const SwarmCanvaComponentBody = () => {
       for (let j = 0; j < SQRT_BALL_AMOUNT; ++j) {
         coreCirlce.theta += j * 20;
         coreCirlce.phi += i * 20;
+        // randomize radius between 2 and 6 so balls don't all spawn on top of each other
+        coreCirlce.radius = 2 + Math.random() * 4;
         coreCirlceVect.setFromSpherical(coreCirlce);
 
         inst.push({
@@ -49,8 +54,7 @@ const SwarmCanvaComponentBody = () => {
     return inst;
   }, []);
 
-  // Apply gravitational impulses every frame
-  useFrame((_rs, _delta) => {
+  useBeforePhysicsStep(() => {
     if (!ballsRigidBodiesRef.current || !coreRef.current) return;
 
     // rotate the core slowly
@@ -62,10 +66,9 @@ const SwarmCanvaComponentBody = () => {
       const t = ballRB.translation();
       _pos.set(t.x, t.y, t.z);
       const dist = _pos.distanceTo(GRAVITY_CENTER);
-      if (dist < 0.001) continue; // skip bodies at origin
 
       // Newtonian-ish force
-      const scale = (GFORCE * CORE_MASS * SPHERE_MASS) / Math.pow(_pos.distanceTo(GRAVITY_CENTER), 2);
+      const scale = (GFORCE * CORE_MASS * SPHERE_MASS) / Math.pow(dist, 2);
       _impulse.copy(GRAVITY_CENTER).sub(_pos).normalize().multiplyScalar(scale).normalize();
       ballRB.applyImpulse(_impulse, true);
     }
@@ -73,33 +76,37 @@ const SwarmCanvaComponentBody = () => {
 
   return (
     <>
+      <RigidBody
+        type='fixed'
+        ref={coreRef}
+        position={[0, 0, 0]}
+        colliders={false}
+      >
+        <boxGeometry args={[1.2, 1.2, 1.2]} />
+        <CuboidCollider args={[0.6, 0.6, 0.6]} restitution={OBJECT_RESTITUTION} density={CORE_DENSITY}/>
+      </RigidBody>
+      <InstancedRigidBodies
+        type='dynamic'
+        ref={ballsRigidBodiesRef}
+        instances={instances}
+        colliders={false}
+        colliderNodes={[<BallCollider args={[SPHERE_RADIUS]} restitution={OBJECT_RESTITUTION} density={SPHERE_DENSITY}/>]}
+      >
+        <instancedMesh args={[undefined, undefined, COUNT]} castShadow receiveShadow>
+          <sphereGeometry args={[SPHERE_RADIUS, 16, 16]} />
+          <meshPhongMaterial color={0xdcdbd7} />
+        </instancedMesh>
+      </InstancedRigidBodies>
+    </>
+  );
+};
+
+const SwarmCanvaComponentBody = () => { 
+  return (
+    <>
       <pointLight color={0xFFFFFF} position={[0, -10, 0]} intensity={1} />
-      <Physics gravity={[GRAVITY_CENTER.x, GRAVITY_CENTER.y, GRAVITY_CENTER.z]} timeStep="vary">
-        <RigidBody
-          type='fixed'
-          ref={coreRef}
-          position={[0, 0, 0]}
-          mass={CORE_MASS}
-          restitution={OBJECT_RESTITUTION}
-          colliders={false}
-        >
-          <boxGeometry args={[1.2, 1.2, 1.2]} />
-          <CuboidCollider args={[0.6, 0.6, 0.6]} />
-        </RigidBody>
-        <InstancedRigidBodies
-          type='dynamic'
-          ref={ballsRigidBodiesRef}
-          instances={instances}
-          colliders={false}
-          colliderNodes={[<BallCollider args={[SPHERE_RADIUS]} />]}
-          mass={SPHERE_MASS}
-          restitution={OBJECT_RESTITUTION}
-        >
-          <instancedMesh args={[undefined, undefined, COUNT]} castShadow receiveShadow>
-            <sphereGeometry args={[SPHERE_RADIUS, 16, 16]} />
-            <meshPhongMaterial color={0xdcdbd7} />
-          </instancedMesh>
-        </InstancedRigidBodies>
+      <Physics gravity={[GRAVITY_CENTER.x, GRAVITY_CENTER.y, GRAVITY_CENTER.z]}>
+        <SwarmScene />
       </Physics>
     </>
   );
@@ -108,14 +115,12 @@ const SwarmCanvaComponentBody = () => {
 const SwarmCanvaComponent = () => {
   return (
     <div style={{ width: "100%", height: "100%", touchAction: "none" }}>
-      <Canvas 
-        shadows={{ type: PCFSoftShadowMap }}>
-        <PerspectiveCamera makeDefault position={[0, -5, 0]} fov={75} near={0.1} far={1000}>
-          <directionalLight color={0xffffff} intensity={0.1} castShadow />
-        </PerspectiveCamera>
-        <scene background={BG_COLOR}>
-          <SwarmCanvaComponentBody />
-        </scene>
+      <Canvas
+        shadows={{ type: PCFSoftShadowMap }}
+        camera={{ fov: 75, near: 0.1, far: 1000, position: [0, -5, 0] }}
+        onCreated={({ camera }) => camera.lookAt(0, 0, 0)}
+      >
+        <SwarmCanvaComponentBody />
       </Canvas>
     </div>
   );
